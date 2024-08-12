98c75f348f30f177a5282234eea15adc0da93e62
kixks <kenmentzy@gmail.com>
2024-08-12 15:56:03 +0800

    automatic populate activityLog data. every action

--- Code.js
+++ Code.js
@@ -633,6 +633,8 @@ function transferNotifToStartandEndDate() {
       sheet.getRange(rowNum, copiedStatusIndex + 1).setValue('Yes'); // Adding 1 to convert zero-based index to 1-based index
     });
 
+    addToActivityLog('SystemStart',rowsToCopy[0][0], "");
+
   } else {
     Logger.log('No rows to copy or date is already on the Start Date');
   }
@@ -641,6 +643,7 @@ function transferNotifToStartandEndDate() {
     endDateSheet.getRange(endDateSheet.getLastRow() + 1, 1, rowsToTransfer.length, rowsToTransfer[0].length).setValues(rowsToTransfer);
     Logger.log(`${rowsToTransfer.length} rows copied to End Date.`);
 
+    addToActivityLog('SystemEnd',rowsToTransfer[0][0], "");
     //Delete the rows from the notif and start sheet
     rowsToDelete.forEach(row => {
       const rowIndex = data.indexOf(row) + 3; // Adding 2 to account for the header row and zero-based index
@@ -759,4 +762,42 @@ function formatDates(dateString) {
 
 function getScriptUrl() {
   return ScriptApp.getService().getUrl();
+}
+
+function addToActivityLog(activityType, troubleTicket, incidentTypeAdd){
+  const ss = SpreadsheetApp.openById('1vW8zgcrQC02iRLkWJSOIjfnqN5_lRNMgNjV6IBZF__c');
+  var activitySheet = ss.getSheetByName('Activity History');
+
+  var currentDate = getCurrentDateTableFormat();
+  var notifToPush = "";
+  var personsEmail = Session.getActiveUser().getEmail();
+  if(activityType === 'Add'){
+    var notifToPush = personsEmail + ' has Added a New ' + incidentTypeAdd +' Ticket ( '+troubleTicket +" )";
+  }
+  else if(activityType === 'Delete'){
+    var notifToPush = personsEmail + ' has Deleted an Existing Ticket ( '+troubleTicket +' )';
+  }
+  else if(activityType === "Update"){
+    var notifToPush = personsEmail + ' has Updated the Ticket ( '+troubleTicket +' )';
+  }
+  else if(activityType === "toStartDate"){
+    var notifToPush = personsEmail + ' has Copied the Ticket to Start Date. ( '+troubleTicket +' )';
+  }
+  else if(activityType === "toEndDate"){
+    var notifToPush = personsEmail + ' has Sent the Ticket to End Date ( '+troubleTicket +' )';
+  }
+  else if(activityType === "SystemStart"){
+    var notifToPush = 'The system automatically copied the ticket to Start Date ( '+troubleTicket +' )';
+  }
+  else if(activityType === "SystemEnd"){
+    var notifToPush = 'The system automatically send the ticket to End Date ( '+troubleTicket +' )';
+  }
+
+  var rowData = [[notifToPush, currentDate]];
+
+  var lastRow = activitySheet.getLastRow();
+  var range = activitySheet.getRange(lastRow+ 1,1,1,2);
+  range.setValues(rowData);
+
+  activitySheet.getRange(lastRow + 1, 2).setNumberFormat('yyyy-mm-dd hh:mm');
 }
